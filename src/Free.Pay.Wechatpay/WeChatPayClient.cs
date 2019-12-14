203daf7f6405aef44858e9e77fb79f9c3ea20506
辉 冯 <695979933@qq.com>
2019-12-14 23:19:40 +0800

    add

---
+++ src/Free.Pay.Wechatpay/WeChatPayClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Free.Pay.Wechatpay
+{
+    public class WeChatPayClient:IWeChatPayClient
+    {
+        /// <summary>
+        ///     Execute WeChatPay API request implementation
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public Task<T> ExecuteAsync<T>(T request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
