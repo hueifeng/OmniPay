13e8daadd7443a00f1d3225610d0abd669b63f1d
辉 冯 <695979933@qq.com>
2019-12-15 16:47:30 +0800

    GetParameters

--- src/Free.Pay.Core/Request/BaseRequest.cs
+++ src/Free.Pay.Core/Request/BaseRequest.cs
@@ -1,9 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Free.Pay.Core.Request
 {
     public abstract class BaseRequest<TModel,Response> 
     {
         public string RequestUrl { get; set; }
+        /// <summary>
+        ///     获取所有Key-value形式的文本请求参数字典。
+        /// </summary>
+        /// <returns></returns>
+        public IDictionary<string, string> GetParameters()
+        {
+            throw new NotImplementedException("stack not Implement");
+        }
+
     }
 }
