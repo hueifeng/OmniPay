e81ce7f0b4dcde19afa580fd69bb514bbfdb8f3d
冯辉 <695979933@qq.com>
2020-01-27 20:51:40 +0800

    Update

--- test/Wechatpay.Test/Endpoints/WechatScanPayEndpointTest.cs
+++ test/Wechatpay.Test/Endpoints/WechatScanPayEndpointTest.cs
@@ -1,50 +1,49 @@
-using Xunit;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Free.Pay.Wechatpay.Endpoints;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using FluentAssertions;
 using Free.Pay.Core.Results;
+using Free.Pay.Wechatpay.Endpoints;
 using Free.Pay.Wechatpay.Endpoints.Result;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Xunit;
 
-namespace Free.Pay.Wechatpay.Test.Endpoints
-{
-    public class WechatScanPayEndpointTest
-    {
-        HttpContext _context= new DefaultHttpContext();
-        private ILogger<WeChatPayClient> wechatpaylogger = new LoggerFactory().CreateLogger<WeChatPayClient>();     
-        private ILogger<WechatScanPayEndpoint> logger = new LoggerFactory().CreateLogger<WechatScanPayEndpoint>();
+namespace Free.Pay.Wechatpay.Test.Endpoints {
+    public class WechatScanPayEndpointTest {
+        HttpContext _context = new DefaultHttpContext ();
+        private ILogger<WeChatPayClient> wechatpaylogger = new LoggerFactory ().CreateLogger<WeChatPayClient> ();
+        private ILogger<WechatScanPayEndpoint> logger = new LoggerFactory ().CreateLogger<WechatScanPayEndpoint> ();
+        IWeChatPayClient _client;
+        private WechatScanPayEndpoint _subject;
 
-         IWeChatPayClient _client;
-         private WechatScanPayEndpoint _subject;
-
-        private void init(){
-            var someOptions = Options.Create(new WeChatPayOptions{
-                AppId="wxdace645e0bc2c424",
-                AppSecret="4693afc6b2084885ca9fbc2355b97827",
-                BaseUrl="https://api.mch.weixin.qq.com",
-                Key="1900009641"
+        private void init () {
+            var someOptions = Options.Create (new WeChatPayOptions {
+                AppId = "wxdace645e0bc2c424",
+                    AppSecret = "4693afc6b2084885ca9fbc2355b97827",
+                    BaseUrl = "https://api.mch.weixin.qq.com",
+                    Key = "1900009641"
             });
-            _client=new WeChatPayClient(someOptions,wechatpaylogger);
-            _subject=new WechatScanPayEndpoint(logger,_client);
+            _client = new WeChatPayClient (someOptions, wechatpaylogger);
+            _subject = new WechatScanPayEndpoint (logger, _client);
         }
-        public WechatScanPayEndpointTest(){
-            this.init();
+        public WechatScanPayEndpointTest () {
+            this.init ();
         }
+
         [Fact]
-        public void Process_get_entry_point_shoud_return_405(){
-            _context.Request.Method="GET";
-            var result=_subject.Process(_context);
+        public void Process_get_entry_point_shoud_return_405 () {
+            _context.Request.Method = "GET";
+            var result = _subject.Process (_context);
             var statusCode = result as StatusCodeResult;
-            statusCode.Should().NotBeNull();
-            statusCode.StatusCode.Should().Be(405);
+            statusCode.Should ().NotBeNull ();
+            statusCode.StatusCode.Should ().Be (405);
         }
+
         [Fact]
-        public void Process_scanpay_path_should_return_sanpay_result(){
-            _context.Request.Method="POST";
-            var result=_subject.Process(_context);
-            result.Should().BeOfType<ScanPayResult>();
+        public void Process_scanpay_path_should_return_sanpay_result () {
+            _context.Request.Method = "POST";
+            var result = _subject.Process (_context);
+            result.Should ().BeOfType<ScanPayResult> ();
         }
     }
 }
