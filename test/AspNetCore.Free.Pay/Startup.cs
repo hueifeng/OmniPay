b80ddfb3d61787aa482cbc08b41edcad8038201f
冯辉 <695979933@qq.com>
2020-01-31 17:15:56 +0800

    Update Startup.cs

--- test/AspNetCore.Free.Pay/Startup.cs
+++ test/AspNetCore.Free.Pay/Startup.cs
@@ -26,7 +26,6 @@ namespace AspNetCore.Free.Pay
             });
             services.AddControllersWithViews();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
