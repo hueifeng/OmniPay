46b0abe2b9c592dec03396351fe73a9b9f85e0c1
HueiFeng <695979933@qq.com>
2020-06-15 17:52:03 +0800

    Added AlipayServiceCollectionExtensions

---
+++ src/Free.Pay.Alipay/Extensions/AlipayServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Free.Pay.Alipay.Extensions
+{
+    public static class AlipayServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAliPay(this IServiceCollection services, Action<AlipayOptions> action)
+        {
+            return services.AddAliServices(action);
+        }
+
+        private static IServiceCollection AddAliServices(this IServiceCollection services, Action<AlipayOptions> action)
+        {
+            if (action != null)
+            {
+                services.Configure<AlipayOptions>(action.Invoke);
+            }
+            return services;
+        }
+    }
+}
